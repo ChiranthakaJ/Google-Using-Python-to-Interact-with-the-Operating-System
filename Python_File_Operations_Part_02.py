1ba78235bc06132dadec710b4df8d9f0891252c9
chiranthakaj <chiransj@gmail.com>
2020-10-10 09:28:05 +0530

    Second Commit

--- Python_File_Operations_Part_02.py
+++ Python_File_Operations_Part_02.py
@@ -4,7 +4,8 @@
 
 #To overcome this in Python we can have a different approach using the 'with' keyword.
 
-with open("Python_File_Sample_V2.txt") as file: 
+with open("Python_File_Sample_V2.txt") as file: #This path is a relative path, which doesn't want to say the drive location and the root folder the file in. This is more accurate and error free method.
+    
     print(file.read())
     
 #With this approach, at the end of the execution the file will automatically closed.
