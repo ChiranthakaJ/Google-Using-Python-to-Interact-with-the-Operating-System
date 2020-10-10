29f2aaf4f602595c1bac5bf65625d1de6279aa26
chiranthakaj <chiransj@gmail.com>
2020-10-10 09:27:24 +0530

    Second Commit

--- Python_File_Operations_Part_01.py
+++ Python_File_Operations_Part_01.py
@@ -1,7 +1,7 @@
 #Example 01 - File openning and reading the content. Once done that closing.
 
 #open() method is used to open a file in Python. Once you open the file the content will be available and the content needs to be assign to a variable in order to read. In here 'file' is the variable that holds the content once the file openned. 
-file = open("Python_File_Sample_V1.txt")   
+file = open("Python_File_Sample_V1.txt")  #This path is a relative path, which doesn't want to say the drive location and the root folder the file in. This is more accurate and error free method.
 
 #readline() method used to read the content line by line. If this method executes one time, then the file buffer move to the next line.
 print(file.readline())  #This will show the first line of the file.
