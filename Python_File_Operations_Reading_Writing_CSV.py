2c878996c402cb59dcc63068ab182ee3a106dfed
chiranthakaj <chiransj@gmail.com>
2020-10-11 10:33:14 +0530

    Second commit

--- Python_File_Operations_Reading_Writing_CSV.py
+++ Python_File_Operations_Reading_Writing_CSV.py
@@ -60,3 +60,90 @@ Lio Nelson,lion,User Experiance Research
 
 Charlie Grey,greyc,Development
 '''
+
+#Example 01 - We're working with a list of flowers and some information about each one. The create_file function writes this information to a CSV file. The contents_of_file function reads this file into records and returns the information in a nicely formatted block. Fill in the gaps of the contents_of_file function to turn the data in the CSV file into a dictionary using DictReader.
+
+import os
+import csv
+
+# Create a file with data in it
+def create_file(filename):
+  with open(filename, "w") as file:
+    file.write("name,color,type\n")
+    file.write("carnation,pink,annual\n")
+    file.write("daffodil,yellow,perennial\n")
+    file.write("iris,blue,perennial\n")
+    file.write("poinsettia,red,perennial\n")
+    file.write("sunflower,yellow,annual\n")
+
+# Read the file contents and format the information about each row
+def contents_of_file(filename):
+  return_string = ""
+
+  # Call the function to create the file 
+  create_file(filename)
+
+  # Open the file 
+  with open(filename) as file:
+    # Read the rows of the file into a dictionary
+    f = csv.DictReader(file)
+    # Process each item of the dictionary
+    for row in f:
+      return_string += "a {} {} is {}\n".format(row["color"], row["name"], row["type"])
+  return return_string
+
+#Call the function
+print(contents_of_file("flowers.csv"))
+
+'''
+a pink carnation is annual
+a yellow daffodil is perennial
+a blue iris is perennial
+a red poinsettia is perennial
+a yellow sunflower is annual
+'''
+
+#Example 02 - Using the CSV file of flowers again, fill in the gaps of the contents_of_file function to process the data without turning it into a dictionary. How do you skip over the header record with the field names?
+
+import os
+import csv
+
+# Create a file with data in it
+def create_file(filename):
+  with open(filename, "w") as file:
+    file.write("name,color,type\n")
+    file.write("carnation,pink,annual\n")
+    file.write("daffodil,yellow,perennial\n")
+    file.write("iris,blue,perennial\n")
+    file.write("poinsettia,red,perennial\n")
+    file.write("sunflower,yellow,annual\n")
+
+# Read the file contents and format the information about each row
+def contents_of_file(filename):
+  return_string = ""
+
+  # Call the function to create the file 
+  create_file(filename)
+
+  # Open the file
+  with open(filename) as f:
+    # Read the rows of the file
+    rows = csv.reader(f)
+    headers = next(rows)
+    # Process each row
+    for row in rows:
+      name, color, ty = row
+      # Format the return string for data rows only
+      return_string += "a {} {} is {}\n".format(color, name, ty)
+  return return_string
+
+#Call the function
+print(contents_of_file("flowers.csv"))
+
+'''
+a pink carnation is annual
+a yellow daffodil is perennial
+a blue iris is perennial
+a red poinsettia is perennial
+a yellow sunflower is annual
+'''
