2d269a0a1762e1d2acb238202d940b5bd5950fe9
chiranthakaj <chiransj@gmail.com>
2020-10-18 23:43:11 +0530

    First commit

---
+++ Python_Managing_Data_And_Processes_Introduction.py
@@ -0,0 +1,13 @@
+# In this module, we'll check out concepts that help us interact with the running operating system. 
+
+#We'll look at how we can get the most out of the tools available. We'll kick this off by talking about how to read user data interactively. 
+
+#Then we'll explore the standard input and output data streams provided by the operating system and see how we can interact with them both from Python programs and from the system programs. 
+
+#In a lot of our examples, we'll interact with the operating system through the command line. So we'll start familiarizing ourselves a bit more with the Linux shell. 
+
+#We'll also talk about how we can execute system commands from Python. This allows our scripts to harness the power of the rest of the operating system. So it's important that we know how to manipulate the output of these commands and handle any generated errors. 
+
+#We'll wrap up by diving into one important source of information for IT specialist, log files. We already got a peek at a few log files in some of our earlier videos. 
+
+#Now, we'll look at how we can make sense of information these log files provide by using the tools that we've learned throughout the course. So here's a bunch of interesting things ahead of us. Are you excited? Because I'm excited. So what are we waiting for? Let's dive in.
